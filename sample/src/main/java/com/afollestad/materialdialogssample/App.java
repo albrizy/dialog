6dc47c6a592a5240aed0a348a14ef016ab4cb5dd
albrizy <albrizy@gmail.com>
2020-02-13 10:39:22 +0700

    Initial commit

---
+++ sample/src/main/java/com/afollestad/materialdialogssample/App.java
@@ -0,0 +1,12 @@
+package com.afollestad.materialdialogssample;
+
+import android.app.Application;
+
+/** @author Aidan Follestad (afollestad) */
+public class App extends Application {
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+  }
+}
